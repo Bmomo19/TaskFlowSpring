060bfd987cc5d967caf25c00db35f55b9df45803
Mohammed <bouraima224@gmail.com>
2025-03-24 09:39:50 +0000

    check

--- src/main/java/com/taskflow/taskflowspring/entity/User.java
+++ src/main/java/com/taskflow/taskflowspring/entity/User.java
@@ -9,8 +9,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.util.Date;
 
 @Entity
-@Table(name = "users")
-@Data
+@NoArgsConstructor
+@ToString
 @Getter
 @Setter
 public class User {
@@ -33,14 +33,13 @@ public class User {
 
     @Basic(optional = false)
     @CreationTimestamp
-    @Column(name = "created_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt = new Date(); // initialize created date
 
     @UpdateTimestamp
-    @Column(name = "updated_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date updatedAt = new Date();
+
 }
 
 
